ec09c8a44208244112275c3d7d130ef154ab9771
irineumouracaldeira <121524120+irineumouracaldeira@users.noreply.github.com>
2024-08-14 16:40:25 -0300

    Add files via upload

---
+++ BalancoTrimestral.java
@@ -0,0 +1,25 @@
+class BalancoTrimestral {
+	public static void main (String[] args) {
+	// Declare variável inteira gastosJaneiro inicialize-a com 15000
+
+	int gastosJaneiro = 15000;
+
+	// Declare variável inteira gastosFevereiro inicialize-a com 23000
+
+	int gastosFevereiro = 23000;
+
+	// Declare variável inteira gastosMarco inicialize-a com 17000
+
+	int gastosMarco = 17000;
+
+	// Crie uma variável chamada gastosTrimestre e inicialize-a com a soma das outras 3 variáveis
+
+	int gastosTrimestre = gastosJaneiro + gastosFevereiro + gastosMarco;
+
+	// imprima variável gastosTrimestre
+
+	System.out.println(gastosTrimestre);
+
+	}
+}
+ 
